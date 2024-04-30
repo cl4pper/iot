758560761d17135480bd6ba84746f30736f14d3c
cl4pper <cl4pper@gmail.com>
2024-04-30 22:22:03 +0100

    feat(t-001): build spaceship_interface code

--- spaceship_interface/src/main.cpp
+++ spaceship_interface/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int switchState = 0;
+int buttonPin = 2;
+int greenLedPin = 3;
+int redLedPrimaryPin = 4;
+int redLedSecondaryPin = 5;
+
+void switchLeds(uint8_t, uint8_t, uint8_t);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // OUTPUTS
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(redLedPrimaryPin, OUTPUT);
+  pinMode(redLedSecondaryPin, OUTPUT);
+  // INPUTS
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switchState = digitalRead(buttonPin);
+
+  if(switchState == LOW) {
+    switchLeds(HIGH, LOW, LOW);
+  } else {
+    switchLeds(LOW, LOW, HIGH);
+
+    delay(250);
+    digitalWrite(redLedPrimaryPin, HIGH);
+    digitalWrite(redLedSecondaryPin, LOW);
+    delay(250);
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void switchLeds(uint8_t valueA, uint8_t valueB, uint8_t valueC) {
+  digitalWrite(greenLedPin, valueA);
+  digitalWrite(redLedPrimaryPin, valueB);
+  digitalWrite(redLedSecondaryPin, valueC);
 }
